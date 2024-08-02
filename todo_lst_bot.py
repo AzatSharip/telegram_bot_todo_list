e4ee13bffd4c5fca556b7e5b19e08e5d27bb8754
Az.Sharip <azat.sharip@gmail.com>
2024-08-03 00:07:53 +0500

    stat button for admins, resized buttons, logging

--- todo_lst_bot.py
+++ todo_lst_bot.py
@@ -1,16 +1,26 @@
-import logging
 import psycopg2
 from psycopg2 import sql
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 
+# Установка ротации логов
+log_handler = RotatingFileHandler(
+    'todo_list_bot.log',         # Имя файла для логов
+    maxBytes=5*1024*1024,  # Максимальный размер файла лога (5 MB)
+    backupCount=3         # Количество резервных копий файлов логов
+)
 
-# Настройка логирования
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    handlers=[log_handler],
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 logger = logging.getLogger(__name__)
-stand = 'prod'
 
+stand = 'prod'
 if stand == 'prod':
     with open("prod_config.json", "r") as config_file:
         config = json.load(config_file)
@@ -24,7 +34,7 @@ DB_NAME = config["DB_NAME"]
 DB_USER = config["DB_USER"]
 DB_PASSWORD = config["DB_PASSWORD"]
 token = config["token"]
-
+admins = config["admins"]
 
 
 # Подключение к базе данных
@@ -39,19 +49,33 @@ def get_db_connection():
 
 # Функция старта
 def start(update: Update, context: CallbackContext) -> None:
-    reply_keyboard = [['Вывести список'], ['Удалить запись']]
+    user_id = update.message.from_user.id
+    user_first_name = update.message.from_user.first_name
+    user_name = update.message.from_user.username
+
+    if user_id in admins:
+        reply_keyboard = [['Вывести список'], ['Удалить запись'], ['Статистика']]
+    else:
+        reply_keyboard = [['Вывести список'], ['Удалить запись']]
+
+    # Логирование начала команды
+    logger.info(f'Пользователь {user_first_name} - {user_name} - ({user_id}) начал сессию.')
+
+
     update.message.reply_text(
-        'Привет! Отправь мне сообщение, и я запишу его в базу данных.',
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        'Привет! Отправь мне сообщение, и я запишу его в базу данных. Номер перед записью писать не надо, только текст!',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=False)
     )
 
 # Функция для записи сообщения в базу данных
 def save_message(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
+    user_name = update.message.from_user.username
     message_text = update.message.text
+    
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO checklist (entry, user_id) VALUES (%s, %s)", (message_text, user_id))
+    cursor.execute("INSERT INTO checklist (entry, user_id, username) VALUES (%s, %s, %s)", (message_text, user_id, user_name))
     conn.commit()
     cursor.execute(f"SELECT reorder_entry_id({user_id})")
     conn.commit()
@@ -91,12 +115,42 @@ def list_messages(update: Update, context: CallbackContext) -> None:
 def delete_message(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Введите № записи, которую хотите удалить или напишите "Отмена":')
     context.user_data['awaiting_delete_id'] = True
-    
+
+# Функция админа
+def statistic(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.from_user.id
+    if user_id in admins:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(f"""
+                            SELECT user_id, count(entry_id)
+                            FROM checklist
+                            group by user_id
+                            order by count(entry_id) desc
+                                            """)
+        results = cursor.fetchall()
+        conn.commit()
+        if results:
+            results = "\n".join([f"{r[0]} - {r[1]}" for r in results])
+            results = "`" + str(results) + "`"
+            update.message.reply_text(results, parse_mode='MarkdownV2')
+        else:
+            update.message.reply_text("Данных нет...")
+    else:
+        update.message.reply_text("Доступа нет!")
+
+
 
 # Обработка текстовых сообщений
 def handle_message(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
+    user_first_name = update.message.from_user.first_name
+    user_last_name = update.message.from_user.last_name
+    user_name = update.message.from_user.username
     text = update.message.text
+
+    logger.info(f'Получено сообщение от {user_first_name} {user_last_name} (@{user_name}): {text}')
+
 
     if text == 'Вывести список':
         list_messages(update, context)
@@ -138,6 +192,8 @@ def handle_message(update: Update, context: CallbackContext) -> None:
                 update.message.reply_text('Пожалуйста, введите корректный номер!')
                 update.message.reply_text('Введите № записи для удаления или напишите "Отмена":')
                 return
+    elif text == 'Статистика':
+        statistic(update, context)
     else:
         save_message(update, context)
 
@@ -151,6 +207,8 @@ def is_numeric(s):
 
 
 def main() -> None:
+    logger.info('Запуск бота')
+
     # Создание updater и диспетчера
     updater = Updater(token)
     dispatcher = updater.dispatcher
